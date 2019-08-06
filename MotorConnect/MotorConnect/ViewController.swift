8252683682ecf9d6bbc043a27240c1ed06fca208
Thanh Ha <thanhahvan1@gmail.com>
2019-08-06 09:51:38 +0700

    init

---
+++ MotorConnect/MotorConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotorConnect
+//
+//  Created by ThanhHV8 on 8/6/19.
+//  Copyright © 2019 VN FOX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
